e984dd62cd27301c40accfdbf58336f03f9611e8
Eundo Lee <cobed95@gmail.com>
2026-03-15 16:33:13 +0900

    P23 solved

--- src/main/scala/solutions/lists/Problem23.scala
+++ src/main/scala/solutions/lists/Problem23.scala
@@ -10,6 +10,19 @@ package solutions.lists
   * res0: List[Symbol] = List('e, 'd, 'a)
   */
 
+import scala.util.Random
+
 object Problem23 {
-  def randomSelect[A](n: Int, list: List[A]): List[A] = ???
+  def randomSelectIter[A]: (Int, Int, List[A]) => List[A] = {
+    case (_, _, Nil) => Nil
+    case (_, n, _ :: _) if n <= 0 => Nil
+    case (l, n, hd :: tl) =>
+      Problem20.removeAt(Random.nextInt(l), hd :: tl) match {
+        case (li, Some(el)) => el :: randomSelectIter(l - 1, n - 1, li)
+        case (_, None) => throw new Exception("Should not reach here")
+      }
+  }
+
+  def randomSelect[A](n: Int, list: List[A]): List[A] =
+    randomSelectIter(Problem04.length(list), n, list)
 }
