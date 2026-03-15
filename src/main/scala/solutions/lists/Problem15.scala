c1e3c563b58be8c086b5232c058aa2c40eda0e39
Andy.lee (앤디) <andy.lee+karrot@daangn.com>
2026-03-15 14:59:14 +0900

    P15 solved

--- src/main/scala/solutions/lists/Problem15.scala
+++ src/main/scala/solutions/lists/Problem15.scala
@@ -9,5 +9,12 @@ package solutions.lists
   */
 
 object Problem15 {
-  def duplicateN[A](n: Int, list: List[A]): List[A] = ???
+  def duplicateNIter[A]: (Int, Int, List[A]) => List[A] = {
+    case (_, _, Nil) => Nil
+    case (n, m, _ :: tl) if m <= 0 => duplicateNIter(n, n, tl)
+    case (n, m, hd :: tl) => hd :: duplicateNIter(n, m - 1, hd :: tl)
+  }
+
+  def duplicateN[A](n: Int, list: List[A]): List[A] =
+    duplicateNIter(n, n, list)
 }
